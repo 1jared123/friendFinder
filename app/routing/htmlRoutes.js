f1946c70b296f92b659f1d877edae3eb1181f1fd
Jared Moscrip <jaredmoscrip@gmail.com>
2017-04-27 13:54:23 -0500

    Things working. more or less

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,22 @@
+var path = require("path");
+var express = require("express");
+var app = express();
+module.exports = function(app) {
+
+app.get("/", function(reqest, response) {
+  response.sendFile(path.join(__dirname, "/../public/home.html"));
+});
+
+app.get("/survey", function(request, response) {
+  response.sendFile(path.join(__dirname, "/../public/survey.html"));
+});
+
+app.post("/friends"), function(request, response) {
+
+}
+
+  // If no matching route is found default to home
+  app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+};
--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 
 var app = express();
 var PORT = 8000;
 
-app.get("/", function(reqest, response) {
-  response.sendFile(path.join(__dirname, "app/public/home.html"));
-});
+app.use(express.static(__dirname + "/app"));
 
-app.get("/survey", function(request, response) {
-	response.sendFile(path.join(__dirname, "app/public/survey.html"));
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
 
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+//we listening to you...
 app.listen(PORT, function() {
-  console.log("Warming the reactors.");
+  console.log("Reactors primed. Fire when ready.");
 });
